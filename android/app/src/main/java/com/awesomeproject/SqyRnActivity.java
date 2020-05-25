440d9cd8f13149511e4809a130518ba4cca3676f
ssqingyuan <sqingyuan@aliyun.com>
2020-05-25 15:42:29 +0800

    fix:修改原声页面按钮的点击事件

--- android/app/src/main/java/com/awesomeproject/SqyRnActivity.java
+++ android/app/src/main/java/com/awesomeproject/SqyRnActivity.java
@@ -3,6 +3,8 @@ package com.awesomeproject;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
@@ -12,29 +14,54 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class SqyRnActivity extends AppCompatActivity {
+public class SqyRnActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sqy_rn);
+        button = findViewById(R.id.btn_submit);
+        button.setOnClickListener(this);
     }
-    private View createView() {
-        LinearLayout ll= new LinearLayout(this);
-        ll.setGravity(Gravity.CENTER);
-        ll.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        // 设置文字
-        EditText editText = new EditText(this);
-        Button button = new Button(this);
 
-        TextView mTextView = new TextView(this);
-        mTextView.setText("hello world");
-        LinearLayout.LayoutParams mLayoutParams = new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        // 在父类布局中添加它，及布局样式
-        ll.addView(mTextView, mLayoutParams);
-        return ll;
+    @Override
+    public void onClick(View view){
+        switch(view.getId()){
+            case R.id.btn_submit:
+            onClickButton(view);
+            break;
+            default:
+                break;
+        }
     }
+    private void onClickButton(View view) {
+        //处理逻辑
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("提示");
+        builder.setIcon(R.mipmap.ic_launcher_round);
+        builder.setMessage("确定提交吗？");
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getBaseContext(), "提交成功", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getBaseContext(), "未提交成功", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //builder.setCancelable(true);  点击对话框之外的地方对话框就消失
+        builder.setCancelable(true);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }
+
 
 }
